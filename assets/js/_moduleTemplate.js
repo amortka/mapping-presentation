5795c87d927fad8a9fee4450313ebd5eadc7737e
Adam Mortka <a.mortka@gmail.com>
2016-11-16 14:20:37 +0100

    geo projections transform migrated from d3.js v3 to v4.0

---
+++ assets/js/_moduleTemplate.js
@@ -0,0 +1,20 @@
+// name it after id of section, name of the module should be camelCase with 'module' prefix
+// by Adam Mortka
+
+'use strict';
+var moduleGeoProjectionsIntro = (function() {
+    var count = 0;
+
+    var init = function() {
+        console.log('moduleGeoProjectionsIntro init()');
+    };
+
+    var reset = function() {
+        console.log('moduleGeoProjectionsIntro reset()', count++);
+    };
+
+    return {
+        init: init,
+        reset: reset
+    }
+})();
